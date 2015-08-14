8aabff36c4751e66a5ed35a3eeaca9000c39f3bb
Brandon Poole <brandoncpoole@gmail.com>
2015-08-14 11:41:44 -0400

    Fixed Quadruped calculations

--- src/inventory.js
+++ src/inventory.js
@@ -3,7 +3,7 @@ import {computedFrom} from 'aurelia-framework';
 export class inventory {
   currentLoad = 1;
   strength = 10;
-  creatureType = 'bipedal';
+  creatureType = 'biped';
   creatureSize = 'medium';
 
   @computedFrom('currentLoad', 'strength', 'creatureType', 'creatureSize');
@@ -18,15 +18,13 @@ export class inventory {
   calculateCarryCapacity() {
     var loadSizes = [];
     for (var i = 1; i <= 29; i++) {
-      var modStr = i + this.getCategoryModifier(i);
-
       var light, medium, heavy;
       if (i < 10) {
-        heavy = modStr * 10;
+        heavy = i * 10;
       } else {
         var values = [0,25,28.75,32.5,37.5,43.75,50,57.5,65,75,87.5];
-        var idx = 1 + modStr - 10 * Math.floor(modStr/10);
-        heavy = values[idx] * Math.pow(4, Math.floor(modStr/10));
+        var idx = 1 + i - 10 * Math.floor(i/10);
+        heavy = values[idx] * Math.pow(4, Math.floor(i/10));
       }
 
       heavy = heavy * this.getSizeModifier();
@@ -68,51 +66,47 @@ export class inventory {
   }
 
   getSizeModifier() {
-    switch (this.creatureSize) {
-      case 'fine':
-            return 1/8;
-      case 'diminutive':
-            return 1/4;
-      case 'tiny':
-            return 1/2;
-      case 'small':
-            return 3/4;
-      case 'large':
-            return 2;
-      case 'huge':
-            return 4;
-      case 'gargantuan':
-            return 8;
-      case 'colossal':
-            return 16;
-      default: //medium
-            return 1;
-    }
-  }
-
-  getCategoryModifier(str) {
-    if (this.creatureType === 'bipedal') {
-      return 0;
+    if (this.creatureType === 'biped') {
+      switch (this.creatureSize) {
+        case 'fine':
+          return 1 / 8;
+        case 'diminutive':
+          return 1 / 4;
+        case 'tiny':
+          return 1 / 2;
+        case 'small':
+          return 3 / 4;
+        case 'large':
+          return 2;
+        case 'huge':
+          return 4;
+        case 'gargantuan':
+          return 8;
+        case 'colossal':
+          return 16;
+        default: //medium
+          return 1;
+      }
     } else { //quadruped
       switch (this.creatureSize) {
         case 'fine':
-          return str*(1/4);
+          return 1/4;
         case 'diminutive':
-          return str*(1/2);
+          return 1/2;
         case 'tiny':
-          return str*(3/4);
+          return 3/4;
         case 'small':
-          return str*(1);
+          return 1;
         case 'large':
-          return str*(3);
+          return 3;
         case 'huge':
-          return str*(6);
+          return 6;
         case 'gargantuan':
-          return str*(12);
+          return 12;
         case 'colossal':
-          return str*(24);
+          return 24;
         default: //medium
-          return str*2;
+          return 1.5;
       }
     }
   }

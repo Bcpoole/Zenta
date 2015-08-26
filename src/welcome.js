273ea83d09915f745e530a0e7ca89b68497f0dad
Bcpoole <brandoncpoole@gmail.com>
2015-08-26 12:16:02 -0500

    Read character from file if valid JSON.

--- src/welcome.js
+++ src/welcome.js
@@ -2,4 +2,30 @@ import {computedFrom} from 'aurelia-framework';
 
 export class Welcome{
   heading = 'Pathfinder Utility Application!';
+
+  constructor() {
+    if (window.File && window.FileList && window.FileReader) {
+    }
+    else {
+        alert("Your browser does not support File API. You will be unable to load your character.");
+    }
+  }
+
+  fileSelected() {
+      let reader = new FileReader();
+      let file = this.$event.target.files[0];
+      reader.readAsText(file);
+      console.log(file);
+      reader.onload = () => {
+        let myCharacter = reader.result;
+
+        try {
+          var json = JSON.parse(myCharacter);
+          this.myCharacter = myCharacter;
+        } catch(e) {
+          this.myCharacter = '';
+          alert(file.name + ' is not valid json');
+        }
+      };
+  }
 }

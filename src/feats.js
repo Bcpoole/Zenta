8d74a4df904981074f5b3b8b2ce3990f21c79bb9
Bcpoole <brandoncpoole@gmail.com>
2015-09-02 17:02:05 -0500

    Fixed routing of submenu

--- src/feats.js
+++ src/feats.js
@@ -3,15 +3,17 @@ import {ApplicationState} from './applicationState';
 
 @inject(ApplicationState)
 export class feats {
+  canActivate(params, routeConfig, navigationInstruction) {
+    return (!!this.loadedCharacter);
+  }
+
   constructor(appState) {
       this.appState = appState;
 
       this.loadedCharacter = this.appState.loadedCharacter;
 
-      if (!this.loadedCharacter) {
-        this.err = 'No character loaded!';
-      } else if (!this.loadedCharacter.feats || this.loadedCharacter.feats.selected.length == 0) {
-        this.err = "Loaded character doesn't have any Feats."
+      if (!this.loadedCharacter.feats || this.loadedCharacter.feats.selected.length == 0) {
+        this.err = "Loaded character doesn't have any Feats.";
       }
   }
 }

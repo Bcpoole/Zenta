9d47df9d9b2c5f2bf6ec0b12118b336e8a425675
Bcpoole <brandoncpoole@gmail.com>
2015-09-02 22:27:31 -0500

    Added routing for Traits & Drawbacks, Skills, Characteristics, Inventory, and Equipment

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ export class App {
     config.map([
       { route: '', name: 'welcome', moduleId: 'welcome', nav: false, title:'Welcome' },
       { route: 'carryCapacity', name: 'carryCapacity', moduleId: 'carryCapacity', nav: true, title:'Carry Capacity' },
-      { route: 'feats', name: 'feats', moduleId: 'feats', nav: false, title: 'Feats' }
+      { route: 'feats', name: 'feats', moduleId: 'feats', nav: false, title: 'Feats & Flaws' },
+      { route: 'traits', name: 'traits', moduleId: 'traits', nav: false, title: 'Traits & Drawbacks' },
+      { route: 'skills', name: 'skills', moduleId: 'skills', nav: false, title: 'Skills' },
+      { route: 'characteristics', name: 'characteristics', moduleId: 'characteristics', nav: false, title: 'Characteristics' },
+      { route: 'inventory', name: 'inventory', moduleId: 'inventory', nav: false, title: 'Inventory' },
+      { route: 'equipment', name: 'equipment', moduleId: 'equipment', nav: false, title: 'Equipment' }
     ]);
 
     this.router = router;

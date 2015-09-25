85c5bc0363d34f26529d7f4b8463b2f1352a9337
Bcpoole <brandoncpoole@gmail.com>
2015-09-25 14:00:56 -0500

    Added a dialog to add items to inventory (needs data validation and save feature).

---
+++ src/character/dialogs/addItem.js
@@ -0,0 +1,12 @@
+import {DialogController} from 'aurelia-dialog';
+
+export class AddItem {
+  static inject = [DialogController];
+
+  constructor(controller){
+    this.controller = controller;
+  }
+  activate(item){
+    this.item = item;
+  }
+}
--- src/character/inventory.js
+++ src/character/inventory.js
@@ -1,14 +1,17 @@
 import {inject} from 'aurelia-framework';
 import {ApplicationState} from '../applicationState';
+import {DialogService} from 'aurelia-dialog';
+import {AddItem} from './dialogs/addItem';
 
-@inject(ApplicationState)
+@inject(ApplicationState, DialogService)
 export class inventory {
   canActivate(params, routeConfig, navigationInstruction) {
     return (!!this.loadedCharacter);
   }
 
-  constructor(appState) {
+  constructor(appState, dialogService) {
     this.appState = appState;
+    this.dialogService = dialogService;
 
     this.loadedCharacter = this.appState.loadedCharacter;
     this.characterInventory = this.loadedCharacter.inventory;
@@ -24,5 +27,22 @@ export class inventory {
         this.totalWeight += item.weight;
       }
     }
+  }
+
+  addItem(sender) {
+    let item = {
+      name: '',
+      weight: '',
+      description: '',
+      isEquipped: false
+    };
+    if (sender === 'inventory') {
+      item.equipped = true;
+    }
+    this.dialogService.open({ viewModel: AddItem, model: item }).then(response => {
+      if (!response.wasCancelled) {
+        this.characterInventory.push(response);
+      }
+    });
   }
 }

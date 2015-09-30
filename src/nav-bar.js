f0dbebc7cfb34985596e46be830ae44666ba2ecc
Bcpoole <brandoncpoole@gmail.com>
2015-09-30 10:38:45 -0500

    Added a save feature for loaded character.

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -10,4 +10,17 @@ export class NavBar {
   constructor(appState) {
     this.appState = appState;
   }
+
+  save() {
+    let element = document.createElement('a');
+    				element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(this.appState.loadedCharacter)));
+    				element.setAttribute('download', this.appState.loadedCharacter.name + " - " + (new Date().toLocaleString()) + ".json");
+
+    				element.style.display = 'none';
+    				document.body.appendChild(element);
+
+    				element.click();
+
+    				document.body.removeChild(element);
+  }
 }

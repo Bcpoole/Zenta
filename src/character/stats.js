44ee7225d4831efc9d32758972cf7c2fa98dc925
Bcpoole <brandoncpoole@gmail.com>
2015-10-01 12:52:30 -0500

    Abstracted ability scores in 'stats' to an array to iterate generate table.

--- src/character/stats.js
+++ src/character/stats.js
@@ -7,6 +7,18 @@ export class stats {
     return (!!this.loadedCharacter);
   }
 
+  get scores() {
+    for (let i in this._scores) {
+      if (this._scores[i] != null && this.abilityScores[this._scores[i].name] != this._scores[i].score) {
+        this.abilityScores[this._scores[i].name] = this._scores[i].score;
+      }
+    }
+    return this._scores;
+  }
+  set scores(val) {
+    this._scores = val;
+  }
+
   constructor(appState) {
     this.appState = appState;
 
@@ -15,5 +27,14 @@ export class stats {
     this.abilityScores = this.loadedCharacter.abilityScores;
     this.offenseStats = this.loadedCharacter.stats.offense;
     this.defenseStats = this.loadedCharacter.stats.defense;
+
+    this.scores = [];
+    let keys = Object.keys(this.abilityScores);
+    for (let key of keys) {
+      this.scores.push({
+        name: key,
+        score: this.abilityScores[key]
+      })
+    }
   }
 }

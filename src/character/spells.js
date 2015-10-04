b6c02df2216baa5c52e9b785aad733d76dc75c35
Bcpoole <brandoncpoole@gmail.com>
2015-10-03 21:25:31 -0500

    Fixed Spells UI. Formatting to sampleCharacter.json.

--- src/character/spells.js
+++ src/character/spells.js
@@ -12,5 +12,55 @@ export class spells {
 
     this.loadedCharacter = this.appState.loadedCharacter;
     this.spells = this.loadedCharacter.spells;
+
+    this.assignSpellLevels();
+  }
+
+  assignSpellLevels() {
+    this.spells0 = [];
+    this.spells1 = [];
+    this.spells2 = [];
+    this.spells3 = [];
+    this.spells4 = [];
+    this.spells5 = [];
+    this.spells6 = [];
+    this.spells7 = [];
+    this.spells8 = [];
+    this.spells9 = [];
+
+    for (let i in this.spells) {
+      switch(this.spells[i].level) {
+        case 0:
+          this.spells0.push(this.spells[i]);
+          break;
+        case 1:
+          this.spells1.push(this.spells[i]);
+          break;
+        case 2:
+          this.spells2.push(this.spells[i]);
+          break;
+        case 3:
+          this.spells3.push(this.spells[i]);
+          break;
+        case 4:
+          this.spells4.push(this.spells[i]);
+          break;
+        case 5:
+          this.spells5.push(this.spells[i]);
+          break;
+        case 6:
+          this.spells6.push(this.spells[i]);
+          break;
+        case 7:
+          this.spells7.push(this.spells[i]);
+          break;
+        case 8:
+          this.spells8.push(this.spells[i]);
+          break;
+        case 9:
+          this.spells9.push(this.spells[i]);
+          break;
+      }
+    }
   }
 }

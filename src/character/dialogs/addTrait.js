092104239a9ac41385fcb5f328e84b13c36d7f94
Bcpoole <brandoncpoole@gmail.com>
2015-10-07 11:48:03 -0500

    Added ability to add new traits and drawbacks.

---
+++ src/character/dialogs/addTrait.js
@@ -0,0 +1,12 @@
+import {DialogController} from 'aurelia-dialog';
+
+export class AddTrait {
+  static inject = [DialogController];
+
+  constructor(controller){
+    this.controller = controller;
+  }
+  activate(trait){
+    this.trait = trait;
+  }
+}
--- src/character/traits.js
+++ src/character/traits.js
@@ -1,18 +1,43 @@
 import {inject} from 'aurelia-framework';
 import {ApplicationState} from '../applicationState';
+import {DialogService} from 'aurelia-dialog';
+import {AddTrait} from './dialogs/addTrait';
 
-@inject(ApplicationState)
+@inject(ApplicationState, DialogService)
 export class traits {
   canActivate(params, routeConfig, navigationInstruction) {
     return (!!this.loadedCharacter);
   }
 
-  constructor(appState) {
+  constructor(appState, dialogService) {
     this.appState = appState;
+    this.dialogService = dialogService;
 
     this.loadedCharacter = this.appState.loadedCharacter;
     this.traits = this.loadedCharacter.traits.selected;
     this.drawbacks = this.loadedCharacter.traits.drawbacks;
     this.racialTraits = this.loadedCharacter.traits.racial;
   }
+
+  addTrait(sender) {
+      let trait = {
+        name: '',
+        description: '',
+        flavor: ''
+      };
+      if (sender === 'selected') {
+        trait.type = '';
+      }
+      this.dialogService.open({ viewModel: AddTrait, model: trait }).then(response => {
+        if (!response.wasCancelled) {
+          if (sender === 'selected') {
+            this.traits.push(response);
+          } else if (sender === 'racial') {
+            this.racialTraits.push(response);
+          } else if (sender === 'drawback') {
+            this.drawbacks.push(response);
+          }
+        }
+      });
+    }
 }

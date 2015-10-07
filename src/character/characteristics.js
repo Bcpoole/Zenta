a23d2c19544701ebd8770d4c01f73b993f3ee33d
Bcpoole <brandoncpoole@gmail.com>
2015-10-07 16:35:45 -0500

    Added logic to add languages

--- src/character/characteristics.js
+++ src/character/characteristics.js
@@ -1,15 +1,29 @@
 import {inject} from 'aurelia-framework';
 import {ApplicationState} from '../applicationState';
+import {DialogService} from 'aurelia-dialog';
+import {Prompt} from 'aurelia-dialog/examples/prompt';
 
-@inject(ApplicationState)
+@inject(ApplicationState, DialogService)
 export class characteristics {
   canActivate(params, routeConfig, navigationInstruction) {
     return (!!this.loadedCharacter);
   }
 
-  constructor(appState) {
+  constructor(appState, dialogService) {
     this.appState = appState;
+    this.dialogService = dialogService;
 
     this.loadedCharacter = this.appState.loadedCharacter;
+    this.characteristics = this.loadedCharacter.characteristics;
+    this.speed = this.loadedCharacter.speed;
   }
+
+  addLanguage() {
+      let lang = '';
+      this.dialogService.open({ viewModel: Prompt, model: lang }).then(response => {
+        if (!response.wasCancelled) {
+          this.loadedCharacter.languages.push(response);
+        }
+      });
+    }
 }
